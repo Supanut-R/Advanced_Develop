70dbb04b6f43e6c1f1e016a88246edd56224f13b
Supanut <supanut6801@gmail.com>
2024-07-31 16:54:49 +0700

    Readd .pio to .gitignore

--- .pio/build/esp32doit-devkit-v1/FrameworkArduino/libb64/cencode.c.d
+++
@@ -1,3 +0,0 @@
-.pio/build/esp32doit-devkit-v1/FrameworkArduino/libb64/cencode.c.o: \
- C:/Users/670104/.platformio/packages/framework-arduinoespressif32/cores/esp32/libb64/cencode.c \
- C:/Users/670104/.platformio/packages/framework-arduinoespressif32/cores/esp32/libb64/cencode.h
--- .pio/build/esp32doit-devkit-v1/libd71/WebServer/detail/mimetable.cpp.d
+++
@@ -1,4 +0,0 @@
-.pio/build/esp32doit-devkit-v1/libd71/WebServer/detail/mimetable.cpp.o: \
- C:/Users/670104/.platformio/packages/framework-arduinoespressif32/libraries/WebServer/src/detail/mimetable.cpp \
- C:/Users/670104/.platformio/packages/framework-arduinoespressif32/libraries/WebServer/src/detail/mimetable.h \
- C:/Users/670104/.platformio/packages/framework-arduinoespressif32/cores/esp32/pgmspace.h
